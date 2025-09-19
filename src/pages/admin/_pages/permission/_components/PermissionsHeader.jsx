75d165dea008d4d5838b1703965162d9722dc12d
encodedev <timothyonyea@gmail.com>
2025-09-19 11:17:11 +0100

    Super Admin| Role Premission

---
+++ src/pages/admin/_pages/permission/_components/PermissionsHeader.jsx
@@ -0,0 +1,9 @@
+const PermissionsHeader = () => (
+  <div className="flex flex-col sm:flex-row justify-between mb-6 gap-3">
+    <h1 className="text-2xl md:text-3xl lg:text-4xl text-black font-bold">
+      Permissions
+    </h1>
+  </div>
+);
+
+export default PermissionsHeader;
---
+++ src/pages/admin/_pages/permission/_components/SearchAndButton.jsx
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import { PlusSignIcon, Search02Icon } from "hugeicons-react";
+
+import PermissionModal from "./PermissionsModal";
+import { Button } from "../../../../../components";
+
+const SearchAndButton = ({ onCreateOffer }) => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleAddOffer = (offerData) => {
+    if (onCreateOffer) {
+      onCreateOffer(offerData);
+    }
+    setModalVisible(false);
+  };
+
+  return (
+    <>
+      <div className="relative flex w-full flex-col justify-between gap-3 sm:flex-row sm:items-center">
+        <div className="relative w-full max-w-lg">
+          <div className="pointer-events-none absolute inset-y-0 right-5 flex items-center pl-3">
+            <Search02Icon size={19} className="text-green-700" />
+          </div>
+          <input
+            type="text"
+            placeholder="Search by name"
+            className="block w-full rounded-full bg-green-100 py-3 pr-12 pl-6 font-medium text-green-700 placeholder:text-base placeholder:text-green-700 focus:border-transparent focus:ring-2 focus:ring-green-500 focus:outline-none md:text-lg"
+          />
+        </div>
+
+        <Button
+          variant="primary"
+          size="lg"
+          leftIcon={<PlusSignIcon size={18} />}
+          onClick={() => setModalVisible(true)}
+        >
+          New Permission
+        </Button>
+      </div>
+
+      <PermissionModal
+        visible={modalVisible}
+        onClose={() => setModalVisible(false)}
+        onSubmit={handleAddOffer}
+      />
+    </>
+  );
+};
+
+export default SearchAndButton;
